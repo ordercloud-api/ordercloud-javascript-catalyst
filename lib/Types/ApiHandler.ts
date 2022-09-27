c7024f7b4bc112742e6d19b2caf8b01dfafc2721
Oliver Heywood <oliver.heywood@sitecore.com>
2022-09-27 12:02:50 -0500

    v1.2.0 change ApiHandler return type to match new version of nextjs. Is broader and so backwards compatible

--- lib/Types/ApiHandler.ts
+++ lib/Types/ApiHandler.ts
@@ -1,1 +1,3 @@
-export type ApiHandler = (req: any, res: any, next?: any) => void | Promise<void>;
+export type ApiHandler = (req: any, res: any, next?: any) => unknown | Promise<unknown>; 
+
+// Note that the return type "unknown" is still backwards compatible with the older "void".
